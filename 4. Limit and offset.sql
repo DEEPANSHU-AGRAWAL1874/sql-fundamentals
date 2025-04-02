e2c2c2d0c56638403c2a66612e9d1bd1959080eb
Deepanshu Agrawal <119519129+DEEPANSHU-AGRAWAL1874@users.noreply.github.com>
2025-04-02 12:12:23 +0530

    Create 4. Limit and offset.sql

---
+++ 4. Limit and offset.sql
@@ -0,0 +1,6 @@
+use employees;
+select * from employees;
+select * from employees limit 5;
+select * from employees limit 5 offset 10;
+select * from employees where department = 'Sales' limit 5 offset 3;
+select * from employees where department = 'Sales' limit 5 ;
